2684f0498485566061e37bd160d8ca764144cb81
VitorRT <barberino.italia@gmail.com>
2023-04-20 17:18:52 -0300

    Configuração e ambiente e hello world

---
+++ src/main/java/br/com/nimex/api/APIRestNimex/ApiRestNimexApplication.java
@@ -0,0 +1,13 @@
+package br.com.nimex.api.APIRestNimex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestNimexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestNimexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/nimex/api/APIRestNimex/ApiRestNimexApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.nimex.api.APIRestNimex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestNimexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
